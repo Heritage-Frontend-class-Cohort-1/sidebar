1f374f67ed611ac0785bf3b0fa76b25e2186109d
Alabi Oluwafunmilayo <kkoredesemi@gmail.com>
2026-03-14 14:25:38 +0100

    api

--- src/pages/Services.jsx
+++ src/pages/Services.jsx
@@ -1,10 +1,26 @@
-import React from 'react'
+import { useEffect, useState } from 'react'
+import axios from 'axios'
+
 
 const Services = () => {
+  const[products, setProducts]= useState([]);
+  useEffect(()=>{
+    axios.get("https://fakestoreapi.com/products")
+    .then((res)=>{
+      setProducts(res.data)
+    })
+  }, [])
+
   return (
-    <div>
-        <h1>I render services ranging from web development to mobile app creation</h1>
-        <h2>Also classes are both online and physical</h2>
+    <div>Services
+      {products.map((product)=>(
+        <div key={product.id}>
+          <img src={product.image} width={"150"}  />
+          <h3>{product.title}</h3>
+          <p>{product.description}</p>
+          <p>${product.price}</p>
+        </div>
+      ))}
     </div>
   )
 }
